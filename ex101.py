822a0642103f43a228d10606937fe910bdd1580d
Nicolas Wanderley <nicolaswanderley98@gmail.com>
2025-09-29 14:48:18 -0300

    atualiza

---
+++ ex101.py
@@ -0,0 +1,14 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: Não vota.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATADO.'
+
+
+nasc = int(input('Digite o ano de nascimento: '))
+print(voto(nasc))
